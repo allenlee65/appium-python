bc55f35d9e4b1974371d1b3b91e32946b293d2e6
allenlee65 <allenlee0611@gmail.com>
2025-07-15 20:49:29 +0800

    Update README

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
-####Install Appium
+#### Install Appium
 
-`$ nnpm i --location=global appium`
+`$ npm i --location=global appium`
 
-###Install Appium driver
+#### Install Appium driver
 
 `$ appium driver install uiautomator2`
 
-###Install Appium client library
+#### Create Python Virtual environment
 
-`$ pip install Appium-Python-Client`
+`$ python3 -m vevn vevn`
+`$ source venv/bin/activate`
+
+`$ pip install -r requirements.txt`
+
+#### Start Appium Server
+
+`$ appium`
+
+#### Start Android Vitual Device
+
+`$ adb start-server`
+`$ emulator -list-avds`
+  output result: edium_Phone_API_36.0 (avd name) 
+
+`$ emulator -avd edium_Phone_API_36.0`
+
+#### Run test.py
+
+`$ python test.py`
+
+The Android Settings page shown up and then scroll down to click the Battery section.
+
+<video controls src="Screencast From 2025-07-15 20-44-45.webm" title="Title"></video>
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
-
+from appium.options.android import UiAutomator2Options
 
 capabilities = dict(
     platformName='Android',
@@ -17,13 +17,23 @@ appium_server_url = 'http://localhost:4723'
 
 class TestAppium(unittest.TestCase):
     def setUp(self) -> None:
-        self.driver = webdriver.Remote(appium_server_url, capabilities)
+        options = UiAutomator2Options().load_capabilities(capabilities)
+        self.driver = webdriver.Remote(
+            command_executor=appium_server_url,
+            options=options
+        )
 
     def tearDown(self) -> None:
         if self.driver:
             self.driver.quit()
 
     def test_find_battery(self) -> None:
+        self.driver.implicitly_wait(10)
+        self.driver.swipe(
+            start_x=500, start_y=1500,
+            end_x=500, end_y=300,
+            duration=1000
+        )
         el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
         el.click()
 
